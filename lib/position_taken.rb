34e11a8b078139c06bf84eb0145603a4f943dca1
John Cho <johnycho16@gmail.com>
2019-02-17 17:43:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
-# if any one of those conditions meet, then it return false
-# if none of those conditions meet, then it return true
+# if any one of those conditions meet, then it returns false
+# if none of those conditions meet, then it returns true
 
 def position_taken?(board,index)
   !(board[index] == " " || board[index] == "" || board[index] == nil) 
